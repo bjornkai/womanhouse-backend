0ad04effc3d872a3ccb9aff086e0bb4597e69dec
bjornkai <bjornkairoland@gmail.com>
2019-05-09 19:15:10 -0400

    initial commit to heroku

---
+++ womanhouse-backend/public/precache-manifest.8261ff821fcb568ef971c9d41803fe3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f902cd930de91e5a0ed0e145f86ded74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef341402a11f14f788e",
+    "url": "/static/css/main.5890974c.chunk.css"
+  },
+  {
+    "revision": "dc609c71cca83765f0e8",
+    "url": "/static/js/2.8a9c9d29.chunk.js"
+  },
+  {
+    "revision": "fef341402a11f14f788e",
+    "url": "/static/js/main.9177b704.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
