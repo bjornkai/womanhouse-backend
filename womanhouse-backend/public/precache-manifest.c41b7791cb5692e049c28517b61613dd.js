9259f8187bef77889df189a07c56d8019fcfed35
bjornkai <bjornkairoland@gmail.com>
2019-05-26 14:09:07 -0400

    still working

---
+++ womanhouse-backend/public/precache-manifest.c41b7791cb5692e049c28517b61613dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75857460f2cc57fd13b3f88b7780c19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664af1bf2d7c8f91c247",
+    "url": "/static/css/main.c4c6e4e6.chunk.css"
+  },
+  {
+    "revision": "dc609c71cca83765f0e8",
+    "url": "/static/js/2.8a9c9d29.chunk.js"
+  },
+  {
+    "revision": "664af1bf2d7c8f91c247",
+    "url": "/static/js/main.7abd20d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85731f51ca8ca56e4eb4380b8c244f0d",
+    "url": "/static/media/Calla-Lily-Drawing.85731f51.png"
+  }
+]);
